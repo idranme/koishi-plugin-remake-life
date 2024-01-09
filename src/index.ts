0ea1dafc400a67b31a92f1bf63b6a5ab4f3b1494
idanran <idanran@outlook.com>
2024-01-09 16:14:31 +0800

    1.1.0

--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,14 @@ export interface Config { }
 export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
-  ctx.command('remake', '重开你的人生').action(() => {
+  ctx.command('remake', '重开你的人生').action(({ session }) => {
     const region = randomSelect(regions)
     if (region) {
       const location = randomSelect(locations)
       const identity = randomSelect(identities)
-      return `重开成功！你出生在<b>${region}</b>的<b>${location}</b>，是<b>${identity}</b>！`
+      return `<quote id="${session.messageId}"/>重开成功！你出生在<b>${region}</b>的<b>${location}</b>，是<b>${identity}</b>！`
     } else {
-      return '重开失败！你没能出生！'
+      return `<quote id="${session.messageId}"/>重开失败！你没能出生！`
     }
   })
 }
