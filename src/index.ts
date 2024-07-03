1b7dbac4de8f0a1633615808f9578354b99f09e0
idranme <idranme@outlook.com>
2024-07-03 22:24:50 +0800

    1.1.3

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema, Random } from 'koishi'
 import { regions, locations, identities } from './list'
 
 export const name = 'remake-life'
@@ -9,16 +9,12 @@ export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
   ctx.command('remake', '重开你的人生').action(({ session }) => {
-    const region = randomSelect(regions)
+    const region = Random.pick(regions)
     if (region) {
-      const location = randomSelect(locations)
-      const identity = randomSelect(identities)
+      const location = Random.pick(locations)
+      const identity = Random.pick(identities)
       return `<quote id="${session.messageId}"/>重开成功！你出生在<b>${region}</b>的<b>${location}</b>，是<b>${identity}</b>！`
     }
     return `<quote id="${session.messageId}"/>重开失败！你没能出生！`
   })
-}
-
-function randomSelect<T>(arr: T[]): T {
-  return arr[Math.floor(Math.random() * arr.length)]
 }
