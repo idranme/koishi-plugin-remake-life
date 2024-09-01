9ad0af499c2e493718a2911302524bb23a14eef1
idranme <idranme@outlook.com>
2024-09-01 21:39:46 +0800

    1.2.0

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,36 @@
 import { Context, Schema, Random } from 'koishi'
-import { regions, locations, specialLocations, getSpecialIdentities } from './list'
+import { regions, locations, specialLocations, getSpecialIdentities, specialRegions, identities } from './list'
 
 export const name = 'remake-life'
 
-export interface Config { }
+export interface Config {
+  enableSpecialLocations: boolean
+  enableSpecialIdentities: boolean
+}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.object({
+  enableSpecialLocations: Schema.boolean().default(false).description('是否启用特殊地点，如「幻想乡的博丽神社」'),
+  enableSpecialIdentities: Schema.boolean().default(false).description('是否启用特殊身份，如「博丽灵梦」'),
+})
 
-export function apply(ctx: Context) {
+export function apply(ctx: Context, cfg: Config) {
   ctx.command('remake', '重开你的人生')
-     .alias("重开")
-     .action(({ session }) => {
-    const region = Random.pick(regions)
-    if (region) {
-      let location: string
-      let identity: string
-      if(Object.keys(specialLocations).includes(region)) {
-        // 随机在普通和特殊之间抽取
-        location = Random.pick([
-          ...specialLocations[region],
-          ...locations
-        ])
-      } else {
-        location = Random.pick(locations)
+    .action(({ session }) => {
+      const region = Random.pick(cfg.enableSpecialLocations ? [...regions, ...specialRegions] : regions)
+      if (region) {
+        let location: string
+        if (cfg.enableSpecialLocations && specialLocations[region]) {
+          // 随机在普通和特殊之间抽取
+          location = Random.pick([
+            ...specialLocations[region],
+            ...locations
+          ])
+        } else {
+          location = Random.pick(locations)
+        }
+        const identity = Random.pick(cfg.enableSpecialIdentities ? getSpecialIdentities(location) : identities)
+        return `<quote id="${session.messageId}"/>重开成功！你出生在<b>${region}</b>的<b>${location}</b>，是<b>${identity}</b>！`
       }
-
-      identity = Random.pick(getSpecialIdentities(location))
-
-      return `<quote id="${session.messageId}"/>重开成功！你出生在<b>${region}</b>的<b>${location}</b>，是<b>${identity}</b>！`
-    }
-    return `<quote id="${session.messageId}"/>重开失败！你没能出生！`
-  })
+      return `<quote id="${session.messageId}"/>重开失败！你没能出生！`
+    })
 }
